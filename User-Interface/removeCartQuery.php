f39ec41434844132f56ce5a6386271b339962545
Nikul2004 <123574093+Nikul2004@users.noreply.github.com>
2024-08-26 23:05:40 +0530

    Add files via upload
    This Website are created Using HTML,CSS, java script , php , mysql and also this project are run on XAMPP software to direct run this website

---
+++ User-Interface/removeCartQuery.php
@@ -0,0 +1,32 @@
+	 <?php
+	try{
+
+		// $db = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $user, $pass);
+		$db= new PDO('mysql:host=localhost;dbname=bookstore (1);charset=utf8','root','');
+	
+	}
+	catch(Exception $e){
+		
+		echo "Error has Occured";
+	}
+	
+ 	 
+ 
+if(isset($_POST["removeAll_btn"]))
+	{
+	$id=$_POST["remove_id"];	
+ 	$stmt=$db->prepare("Delete  from cart where Cid=$id ");
+ 	$stmt->execute();
+header("location:cart.php");
+exit();
+}
+if(isset($_POST["remove_btn"]))
+	{
+	$cid=$_POST["remove_cid"];
+ 	$id=$_POST["remove_id"];	
+ 	$stmt=$db->prepare("Delete  from cart where id=$id AND Cid=$cid ");
+ 	$stmt->execute();
+header("location:cart.php");
+exit();
+}
+?>
